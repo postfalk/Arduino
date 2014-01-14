f5ab030afd51710a80cdbf639101f42ba437e359
Falk Schuetzenmeister <schuetzenmeister@berkeley.edu>
2014-01-13 21:22:07 -0800

    add timer library

--- nixie_multiplex/nixie_multiplex.ino
+++ nixie_multiplex/nixie_multiplex.ino
@@ -1,35 +1,27 @@
-# include <avr/interrupt.h>
-# include <avr/io.h>
+#include "TimerOne.h"  
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
+int refreshrate=48;
+     
+void setup() {
+  Serial.begin(9600);
   DDRD = B00111100;
   DDRB = B00001111;
-  PORTD = B0000000;
-  PORTB = B0001000;
-  // setup timer2 to fire every 1ms
-  TCCR2B = 0x00;        //Disbale Timer2 while we set it up
-  TCNT2  = 0;           //Reset Timer Count to 130 out of 255
-  TIFR2  = 0x00;        //Timer2 INT Flag Reg: Clear Timer Overflow Flag
-  TIMSK2 = 0x01;        //Timer2 INT Reg: Timer2 Overflow Interrupt Enable
-  TCCR2A = 0x00;        //Timer2 Control Reg A: Normal port operation, Wave Gen Mode normal
-  TCCR2B = 0x05;        //Timer2 Control Reg B: Timer Prescaler set to 128
+  PORTD = B00000000;
+  PORTB = B00001000;
+  Timer1.initialize(1000000/refreshrate);
+  Timer1.attachInterrupt(update_display);
 }
+     
+void update_display() {
+   Serial.println('refresh');            
+ }
 
-ISR(TIMER2_OVF_vect) {
-  TCNT2 = 0;
-  TIFR2 = 0X00;
-  PORTB = PORTB >> 1;
-  if (PORTB == 0) {
-    PORTB == B0001000;
-  }
-}
 
 void loop(){
   for (int i=0; i<10; i++) {
     PORTD = i * 4;
-    delay(300);
+    Serial.println("sec");
+    delay(1000);
   }
 }
 
