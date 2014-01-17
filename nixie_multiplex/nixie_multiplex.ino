3335920b45f513826a07fd09df8331e8989d0075
Falk Schuetzenmeister <falk_email@yahoo.com>
2014-01-16 21:54:39 -0800

    Nixie Counter works

--- nixie_multiplex/nixie_multiplex.ino
+++ nixie_multiplex/nixie_multiplex.ino
@@ -1,6 +1,9 @@
 #include "TimerOne.h"  
 
-int refreshrate=48;
+int refreshrate=200;
+int counter = 0;
+int digits[4];
+int lit = 0;
      
 void setup() {
   Serial.begin(9600);
@@ -11,17 +14,28 @@ void setup() {
   Timer1.initialize(1000000/refreshrate);
   Timer1.attachInterrupt(update_display);
 }
+
+void create_digits(int number) {
+  for (int p=0; p<4; p++) {
+    digits[p] = int(number/pow(10, p)) % 10;
+  } 
+}
      
 void update_display() {
-   Serial.println('refresh');            
+  if (lit == 4) {
+    lit = 0;
+  }
+  PORTB = B00001000;
+  PORTB = PORTB >> lit;
+  //Serial.println(PORTB);
+  PORTD = digits[lit] * 4;
+  lit++;   
  }
 
-
 void loop(){
-  for (int i=0; i<10; i++) {
-    PORTD = i * 4;
-    Serial.println("sec");
-    delay(1000);
+  for (int i=0; i<10000; i++) {
+    create_digits(i);
+    delay(200);
   }
 }
 
